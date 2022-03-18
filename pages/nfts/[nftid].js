7525dfea58466131ae729cbecf6ad4ae47d91aef
Luke4299 <58140465+Luke4299@users.noreply.github.com>
2022-03-18 16:13:58 +0000

    enter

--- pages/nfts/[nftid].js
+++ pages/nfts/[nftid].js
@@ -24,35 +24,55 @@ const Nft = () => {
 if (!provider) return
 
 const sdk = new ThirdwebSDK(
-    provider.getSigned()
-    ''
+    provider.getSigned(),
+    'https://eth-rinkeby.alchemyapi.io/v2/Nhj3aLHM15v5ZCWbu3nNwjRPD4I0PM2e'
+
 )
-return sdk.getNFTModule()
+return sdk.getNFTModule('0x9829D81795ed44f53C9F7c1DeC72637Ef81c063d')
     }, [provider])
 
     // get all NFT's in the collection 
 
     useEffect(() => {
         if (!nftModule) return
-        ;(async () => [
+        ;(async () => {
         const nfts = await nftModule.getAll()
 
         const selectedNftArray = nft.filter(
-            (nft) => nft.id == router.query.assetId
+            (nft) => nft.id === router.query.assetId
         )
-
+        setSelectNft(selectedNftArray[0])
     })()
 }, [nftModule])
 
+const marketPlaceModule = useMemo(() => {
+if (provider) return
 
-        )
+const sdk = new ThirdwebSDK(
+    provider.getSigner(),
+    'https://eth-rinkeby.alchemyapi.io/v2/Nhj3aLHM15v5ZCWbu3nNwjRPD4I0PM2e'
+)
+return sdk.getMarketplaceModule(
+    '0x767936d65e94E3700A5AacB17302D6906F81647f'
+)
+    }, [provider])
 
 
-        ]
-    }
+useEffect(() => {
+    if (!marketPlaceModule) return
+    ;(async () =>{
+setListings(await marketPlaceModule.getAllListings())
+        })()
+}, [marketPlaceModule])
 
+return (
+<div>
+    <Header />
+    <NFTImage />
 
-
-
-
+    </div>
+)
 }
+
+export default Nft
+
