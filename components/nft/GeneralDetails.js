30e963e4a6dbb34c355a8ef7bc3d91d5ba7cb01e
Luke4299 <58140465+Luke4299@users.noreply.github.com>
2022-05-16 00:12:40 +0100

    enter

--- components/nft/GeneralDetails.js
+++ components/nft/GeneralDetails.js
@@ -20,43 +20,41 @@ const style = {
 }
 
 const GeneralDetails = ({ selectedNft }) => {
-    return (
-      <div className={style.wrapper}>
-        <div className={style.infoContainer}>
-          <div className={style.accent}>Stoney Tigers Club</div>
-          <div className={style.nftTitle}>{selectedNft?.name}</div>
-          <div className={style.otherInfo}>
-            <div className={style.ownedBy}>
-              Owned by <span className={style.accent}>e88vault</span>
-            </div>
-            <div className={style.likes}>
-              <AiFillHeart className={style.likeIcon} /> 2.3K favorites
-            </div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.infoContainer}>
+        <div className={style.accent}>Bored Ape Yacht Club</div>
+        <div className={style.nftTitle}>{selectedNft?.name}</div>
+        <div className={style.otherInfo}>
+          <div className={style.ownedBy}>
+            Owned by <span className={style.accent}>e88vault</span>
           </div>
-        </div>
-        <div className={style.actionButtonsContainer}>
-          <div className={style.actionButtons}>
-            <div className={`${style.actionButton} ml-2`}>
-              <MdRefresh />
-            </div>
-            <div className={style.divider} />
-            <div className={style.actionButton}>
-              <RiShareBoxLine />
-            </div>
-            <div className={style.divider} />
-            <div className={style.actionButton}>
-              <GiShare />
-            </div>
-            <div className={style.divider} />
-            <div className={`${style.actionButton} mr-2`}>
-              <FiMoreVertical />
-            </div>
+          <div className={style.likes}>
+            <AiFillHeart className={style.likeIcon} /> 2.3K favorites
           </div>
         </div>
       </div>
-    )
-  }
-  
-
+      <div className={style.actionButtonsContainer}>
+        <div className={style.actionButtons}>
+          <div className={`${style.actionButton} ml-2`}>
+            <MdRefresh />
+          </div>
+          <div className={style.divider} />
+          <div className={style.actionButton}>
+            <RiShareBoxLine />
+          </div>
+          <div className={style.divider} />
+          <div className={style.actionButton}>
+            <GiShare />
+          </div>
+          <div className={style.divider} />
+          <div className={`${style.actionButton} mr-2`}>
+            <FiMoreVertical />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default GeneralDetails
